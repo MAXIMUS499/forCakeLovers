4c5aca74d470808de4f0b7c503a897ae5c0ab9a9
MAXIMUS499 <maximus4999@gmail.com>
2018-04-21 21:23:41 +0300

    create basic structure of the project

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require("gulp");
+var livereload = require('gulp-livereload');
+
+//Directory configuration
+//You can add any new path at any part of the array
+//folder/*.extension
+
+var directories = ["./*.html", "./assets/js/**/*.js", "assets/css/style.css"];
+
+gulp.task('default', [], function() {
+    livereload.listen();
+    gulp.watch(directories, function(){
+        gulp.src(directories).pipe(livereload());
+    });
+});
